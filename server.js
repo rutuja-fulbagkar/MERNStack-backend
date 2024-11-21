a74be0018483cf14d5a3670960fca73106c9c2dd
rutuja-fulbagkar <rutujafulbagkar123@gmail.com>
2024-11-21 22:58:28 +0530

    sec tym

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { createServer } from "http";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import cors from "cors"; 
+
+// Load environment variables
+dotenv.config();
+// Connect to the database
+connectDB();
+ 
+const app = express();
+// app.get('/', (req, res) => { 
+//     res.send('Hello, World!');
+// })
+// Middleware to parse JSON request bodies
+app.use(express.json());
+// Enable CORS
+app.use(cors());
+// Create an HTTP server
+const httpServer = createServer(app);
+//  Set the port from environment variables
+const PORT = process.env.PORT || 8000;
+// Start the server
+httpServer.listen(PORT, () => {
+  console.log(`Server Running on port ${PORT}`);
+});
+
